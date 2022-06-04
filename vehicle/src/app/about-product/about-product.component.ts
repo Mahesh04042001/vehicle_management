94d5cdfe7cf4333545e217f27172db5f89826b33
Maheshwaran S <mahe2998@global.chainsys.com>
2022-06-04 19:47:07 +0530

    project04-06

--- vehicle/src/app/about-product/about-product.component.ts
+++ vehicle/src/app/about-product/about-product.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutProductComponent implements OnInit {
 
-  constructor() { /* TODO document why this constructor is empty */ }
+  constructor() { /* document why this constructor is empty */ }
 
-  ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
+  ngOnInit(): void { /* document why this method 'ngOnInit' is empty */ }
 
 }
--- vehicle/src/app/home/home.component.ts
+++ vehicle/src/app/home/home.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { /* TODO document why this constructor is empty */  }
+  constructor() { /* document why this constructor is empty */  }
 
   ngOnInit(): void {
-    // TODO document why this method 'ngOnInit' is empty
+    // document why this method 'ngOnInit' is empty
   
   }
 
